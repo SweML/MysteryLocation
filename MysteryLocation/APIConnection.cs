632a465ea325b6eafd04fd9d285ee8be35b43157
Abdulhadi Kikar <hadi33806@gmail.com>
2020-11-04 20:13:19 +0100

    CreatePost class added and post method in APIconnecten

--- MysteryLocation/APIConnection.cs
+++ MysteryLocation/APIConnection.cs
@@ -1,12 +1,14 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Text;
 using Newtonsoft.Json;
 using Xamarin.Forms;
 
 namespace MysteryLocation
 {
-    public class APIConnection 
+    public class APIConnection
     {
         private HttpClient client;
         private Label label;
@@ -20,8 +22,8 @@ namespace MysteryLocation
         public async void RefreshDataAsync() // Get 
         {
             Console.WriteLine("Trying to connect to API");
-        Uri uri = new Uri(string.Format("https://saabstudent2020.azurewebsites.net/observation", string.Empty));
-           // ActivityIndicator activityIndicator = new ActivityIndicator { Color = Color.Orange };
+            Uri uri = new Uri(string.Format("https://saabstudent2020.azurewebsites.net/observation", string.Empty));
+            // ActivityIndicator activityIndicator = new ActivityIndicator { Color = Color.Orange };
             HttpResponseMessage response = await client.GetAsync(uri);
             if (response.IsSuccessStatusCode)
             {
@@ -33,16 +35,30 @@ namespace MysteryLocation
                 Console.WriteLine(posts);
 
             }
-           
+
 
         }
+
+        public async void CreateNewPost()
+        {
+            Console.WriteLine("Trying to connect to API");
+            Uri uri = new Uri(string.Format("https://saabstudent2020.azurewebsites.net/observation", string.Empty));
+            createPost post = new createPost("myfirstpusheverandeverandever...", "very happy days", new Coordinate(100, 150));
+            string json = JsonConvert.SerializeObject(post, Formatting.Indented);
+            StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await client.PostAsync(uri, content);
+        }
+
+
+
 
         private String ConvertJsonToPosts(string JsonString) // This should return the whole list instead of a String. Todo
         {
             List<Post> posts;
             posts = JsonConvert.DeserializeObject<List<Post>>(JsonString); // Converts the Json to Posts
             String everything = "";
-            foreach(Post p in posts){ 
+            foreach (Post p in posts)
+            {
                 Console.WriteLine(p.toString());
                 everything += p.toString();
             }
