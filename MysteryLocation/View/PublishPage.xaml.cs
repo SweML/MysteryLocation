30bd1423ff4a076b23d45291e26c993ead5e4b71
Fredrik Peterson <48535843+funkyfrulle@users.noreply.github.com>
2020-11-20 02:32:33 +0100

    PublishPage används för att skapa en page med allt som behövs för att skapa post. Öppnas med create post på Home/MainPage. Inga endast utseende och navigation + kamera.

---
+++ MysteryLocation/View/PublishPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MysteryLocation.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PublishPage : ContentPage
+    {
+        public PublishPage()
+        {
+            InitializeComponent();
+        }
+        private async void BtnCam_Clicked(object sender, EventArgs e)
+        {
+            Console.WriteLine("Camera button works at least");
+            try
+            {
+                await CrossMedia.Current.Initialize();
+
+                var photo = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions()
+                {
+                    DefaultCamera = CameraDevice.Rear,
+                    Directory = "Xamarin",
+                    SaveToAlbum = true
+                });
+
+                if (photo != null)
+                    imgCam.Source = ImageSource.FromStream(() => { return photo.GetStream(); });
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message.ToString(), "Ok");
+            }
+        }
+        private async void PublishButton_Clicked(object sender, EventArgs e)
+        {
+
+            Console.WriteLine("Publish button works");
+            await Navigation.PopAsync(false);
+
+
+        }
+    }
+}
