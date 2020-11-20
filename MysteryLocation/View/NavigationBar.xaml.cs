75b0f21bfb53df69e2c7310e7acc48977d33cde4
MarkusLjungqvist <ljungan95@hotmail.com>
2020-11-20 10:25:28 +0100

    Modfied NavigationBar

--- MysteryLocation/View/NavigationBar.xaml.cs
+++ MysteryLocation/View/NavigationBar.xaml.cs
@@ -15,10 +15,42 @@ namespace MysteryLocation
         public User user;
         public NavigationBar()
         {
+            pageCreation();
             InitializeComponent();
 
         }
 
-        
+        private void pageCreation()
+        {
+
+            NavigationPage compass = new NavigationPage(new CompassPage());
+            compass.IconImageSource = "compassIcon.png";
+            compass.Title = "Compass";
+            Children.Add(compass);
+
+            NavigationPage navigationPage = new NavigationPage(new SettingsPage());
+            navigationPage.IconImageSource = "settingsIcon.png";
+            navigationPage.Title = "Settings";
+            Children.Add(navigationPage);
+
+
+
+            NavigationPage home = new NavigationPage(new MainPage());
+            home.IconImageSource = "homeIcon.png";
+            home.Title = "Home";
+            Children.Add(home);
+
+            NavigationPage marked = new NavigationPage(new MarkedPostsPage());
+            marked.IconImageSource = "feedIcon.png";
+            marked.Title = "Marked";
+            Children.Add(marked);
+
+            NavigationPage unlocked = new NavigationPage(new UnlockedPostsPage());
+            unlocked.IconImageSource = "unlockedIcon.png";
+            unlocked.Title = "Unlocked";
+            Children.Add(unlocked);
+
+
+        }
     }
 }
